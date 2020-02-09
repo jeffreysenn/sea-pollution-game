73a2355c75fb6e1a687490b2673ee376b2f81b2d
Xiaoyue Chen <xiaoyue_chen@outlook.com>
2020-02-09 17:41:28 +0100

    Pollution, filter and factory attributes

---
+++ Assets/Source/EconomicAttrib.cs
@@ -0,0 +1,12 @@
+﻿[System.Serializable]
+public struct EconomicAttrib
+{
+    public float costToPurchase;
+    public float profitPerTurn;
+
+    public string GetDiscription()
+    {
+        return "Cost: " + costToPurchase.ToString("0.00") + "\n" +
+               "Profit per turn: " + profitPerTurn.ToString("0.00") + "\n";
+    }
+}
---
+++ Assets/Source/FactoryAttrib.cs
@@ -0,0 +1,17 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+[CreateAssetMenu(fileName = "New FactoryAttrib", menuName = "FactoryAttrib")]
+public class FactoryAttrib : ScriptableObject
+{
+    public string factoryName = "Default Factory";
+    public EconomicAttrib economicAttrib = new EconomicAttrib { costToPurchase = 40.0f, profitPerTurn = 10.0f };
+    public PollutionAttrib pollutionAttrib = new PollutionAttrib { pollution = null, emissionPerTurn = 1.0f };
+    
+    public string GetDescription()
+    {
+        return economicAttrib.GetDiscription() + pollutionAttrib.GetDiscription();
+    }
+}
+

174f297c532edbf3825e7d76d3a8bb30998876c9
Xiaoyue Chen <xiaoyue_chen@outlook.com>
2020-02-09 22:51:18 +0100

    Pollution map

---
+++ Assets/Source/PollutionSum.cs
@@ -0,0 +1,11 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PollutionSum : MonoBehaviour
+{
+    Dictionary<Pollution.Type, float> pollutionMap = new Dictionary<Pollution.Type, float> { };
+
+    public void AddPollution(Pollution.Type type, float val) { pollutionMap[type] += val; }
+    public float GetPollution(Pollution.Type type) { return pollutionMap[type]; }
+}

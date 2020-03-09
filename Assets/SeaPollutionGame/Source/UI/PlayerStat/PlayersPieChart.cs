875561ed306b9bd7712a5c34b260b4b17837178f
cpttrvs <cpt_trivius@hotmail.fr>
2020-03-09 16:15:51 +0100

    New Players stats (common pie chart)

---
+++ Assets/SeaPollutionGame/Source/UI/PlayerStat/PlayersPieChart.cs
@@ -0,0 +1,92 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using ChartAndGraph;
+using TMPro;
+using System;
+using UnityEngine.EventSystems;
+
+public class PlayersPieChart : MonoBehaviour, IPointerClickHandler
+{
+    [System.Serializable]
+    class PlayerStatData
+    {
+        public Color color = Color.white;
+        public Material material { get; set; }
+        public float value { get; set; }
+        public string categoryName = "Player";
+    }
+
+    [SerializeField]
+    private PlayerStatData player1;
+    [SerializeField]
+    private PlayerStatData player2;
+
+    [SerializeField]
+    private PieChart pieChart = null;
+    [SerializeField]
+    private PieAnimation pieAnimation = null;
+    [SerializeField]
+    private TextMeshProUGUI textSum = null;
+    [SerializeField]
+    private TextMeshProUGUI textTitle = null;
+
+    public event Action<PlayersPieChart> OnClick;
+
+    private void Start()
+    {
+        Clear();
+        Setup();
+    }
+
+    private void Setup()
+    {
+        player1.material = new Material(Shader.Find("Chart/Canvas/Solid"));
+        player1.material.color = player1.color;
+
+        player2.material = new Material(Shader.Find("Chart/Canvas/Solid"));
+        player2.material.color = player2.color;
+    }
+
+    public void SetTitle(string s)
+    {
+        if (textTitle)
+            textTitle.text = s;
+    }
+
+    public void SetPlayersValue(float p1, float p2)
+    {
+        player1.value = p1;
+        player2.value = p2;
+    }
+
+    public void Draw()
+    {
+        Clear();
+
+        pieChart.DataSource.AddCategory(player1.categoryName, player1.material);
+        pieChart.DataSource.AddCategory(player2.categoryName, player2.material);
+
+        pieChart.DataSource.SetValue(player1.categoryName, player1.value);
+        pieChart.DataSource.SetValue(player2.categoryName, player2.value);
+
+        if(pieAnimation)
+            pieAnimation.Animate();
+
+        if (textSum)
+            textSum.text = Math.Round(player1.value + player2.value, 1).ToString();
+    }
+
+    private void Clear()
+    {
+        pieChart.DataSource.Clear();
+
+        if (textSum)
+            textSum.text = "";
+    }
+
+    public void OnPointerClick(PointerEventData eventData)
+    {
+        OnClick?.Invoke(this);
+    }
+}
---
+++ Assets/SeaPollutionGame/Source/UI/PlayerStat/PlayersStatController.cs
@@ -0,0 +1,121 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using TMPro;
+
+public class PlayersStatController : MonoBehaviour
+{
+    [System.Serializable]
+    class PlayerStat
+    {
+        public TextMeshProUGUI txtCoinsValue;
+        public TextMeshProUGUI txtIncomeValue;
+        public Player player { get; set; }
+    }
+
+    [System.Serializable]
+    class MapType
+    {
+        public PollutionMapType mapType;
+        public string title;
+    }
+
+    [SerializeField]
+    private PlayersPieChart pieChart = null;
+
+    [SerializeField]
+    private PlayerStat player1Stat;
+    [SerializeField]
+    private PlayerStat player2Stat;
+
+    [SerializeField]
+    private List<MapType> typeOrder = null;
+    private int currentTypeIndex = 0;
+
+    private WorldStateManager worldStateManager = null;
+
+    private void Awake()
+    {
+        worldStateManager = WorldStateManager.FindWorldStateManager();
+        if (worldStateManager == null) { Debug.LogError("[PlayersStatController] Start: WorldStateManager not found"); return; }
+    }
+
+    private void Start()
+    {
+        player1Stat.player = UIManager.Instance.player1;
+        player2Stat.player = UIManager.Instance.player2;
+        
+        worldStateManager.AddEndPlayerTurnFinishEventListener(UpdateEndTurn);
+
+        pieChart.OnClick += PieChart_OnClick;
+    }
+
+    private void Update()
+    {
+        UpdateValues();
+    }
+
+    private void PieChart_OnClick(PlayersPieChart chart)
+    {
+        currentTypeIndex = (currentTypeIndex + 1) % typeOrder.Count;
+        UpdatePieChart();
+    }
+
+    private void UpdateEndTurn()
+    {
+        UpdatePieChart();
+        UpdateValues();
+    }
+
+    private void UpdatePieChart()
+    {
+        PollutionMap map1 = worldStateManager.GetPollutionMap(player1Stat.player.id, typeOrder[currentTypeIndex].mapType);
+        float value1 = map1.GetTotalPollution();
+
+        PollutionMap map2 = worldStateManager.GetPollutionMap(player2Stat.player.id, typeOrder[currentTypeIndex].mapType);
+        float value2 = map2.GetTotalPollution();
+
+        if (typeOrder[currentTypeIndex].mapType == PollutionMapType.FILTERED)
+        {
+            value1 *= -1;
+            value2 *= -1;
+        }
+
+        pieChart.SetPlayersValue(value1, value2);
+        pieChart.SetTitle(typeOrder[currentTypeIndex].title);
+
+        pieChart.Draw();
+    }
+
+    private void UpdateValues()
+    {
+        UpdateCoins(player1Stat.txtCoinsValue, worldStateManager.GetMoney(player1Stat.player.id));
+        UpdateCoins(player2Stat.txtCoinsValue, worldStateManager.GetMoney(player2Stat.player.id));
+
+        UpdateIncome(player1Stat.txtIncomeValue, worldStateManager.GetIncome(player1Stat.player.id));
+        UpdateIncome(player2Stat.txtIncomeValue, worldStateManager.GetIncome(player2Stat.player.id));
+    }
+
+    private void UpdateCoins(TextMeshProUGUI txt, float value)
+    {
+        txt.text = value.ToString();
+    }
+
+    private void UpdateIncome(TextMeshProUGUI txt, float value)
+    {
+        if (value == 0) { txt.text = ""; return; }
+
+        string s = "(";
+        if (value > 0)
+        {
+            s += "+" + value;
+        }
+        else
+        {
+            s += value;
+        }
+        s += ")";
+
+        txt.text = s;
+    }
+}

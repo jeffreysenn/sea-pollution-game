bcabd208a9d023199cdd31df74311dfb8980da01
cpttrvs <captaintrivius@gmail.com>
2020-04-19 16:46:39 +0200

    blending skyboxes based on disaster

--- Assets/SeaPollutionGame/Source/Manager/DisasterManager.cs
+++ Assets/SeaPollutionGame/Source/Manager/DisasterManager.cs
@@ -8,11 +8,35 @@ public class DisasterEvent : UnityEvent<Disaster> { }
 
 public class DisasterManager : MonoBehaviour
 {
+    [System.Serializable]
+    struct DisasterSkybox
+    {
+        public string disasterName;
+        public Texture front;
+        public Texture back;
+        public Texture left;
+        public Texture right;
+        public Texture up;
+        public Texture down;
+    }
+
     Disaster[] disasters = null;
     WorldStateManager stateManager = null;
     DisasterEvent disasterEvent = new DisasterEvent { };
     UnityEvent noDisasterEvent = new UnityEvent { };
     CameraManager cameraManager = null;
+
+    [Header("Skyboxes")]
+    [SerializeField]
+    private Material defaultTexture = null;
+    [SerializeField]
+    private Material skyboxTextureReference = null;
+    [SerializeField]
+    private DisasterSkybox defaultSkybox;
+    [SerializeField]
+    private List<DisasterSkybox> disasterSkyboxes = null;
+
+    private Material skyboxTexture = null;
 
     public void SetDisasters(Disaster[] arr)
     {
@@ -27,7 +51,17 @@ public class DisasterManager : MonoBehaviour
     {
         stateManager = FindObjectOfType<WorldStateManager>();
         cameraManager = FindObjectOfType<CameraManager>();
+
+        skyboxTexture = new Material(skyboxTextureReference);
+        RenderSettings.skybox = skyboxTexture;
+        SetSkybox(defaultSkybox, false);
+
         stateManager.AddEndTurnEventListener(GenDisaster);
+    }
+
+    private void OnDestroy()
+    {
+        RenderSettings.skybox = defaultTexture;
     }
 
     void GenDisaster()
@@ -41,17 +75,18 @@ public class DisasterManager : MonoBehaviour
                 float chance = Random.Range(0.0f, 1.0f);
                 if (chance <= disaster.chancePerTurn)
                 {
-                    disasterEvent.Invoke(disaster);
                     OnDisaster(disaster);
                     return;
                 }
             }
-            noDisasterEvent.Invoke();
+            OnNoDisaster();
         }
     }
 
     void OnDisaster(Disaster disaster)
     {
+        disasterEvent.Invoke(disaster);
+
         Health[] healthComps = FindObjectsOfType<Health>();
         foreach (var health in healthComps)
         {
@@ -65,7 +100,102 @@ public class DisasterManager : MonoBehaviour
                 health.AddHealth(-damage, disaster);
             }
         }
+
         cameraManager.SetState(CameraManager.State.CINEMA);
+
+        //change skybox
+        foreach(DisasterSkybox ds in disasterSkyboxes)
+        {
+            if(ds.disasterName == disaster.title)
+            {
+                SetSkybox(ds);
+                break;
+            }
+        }
     }
 
+    void OnNoDisaster()
+    {
+        noDisasterEvent.Invoke();
+
+        SetSkybox(defaultSkybox);
+    }
+
+    void SetSkybox(DisasterSkybox ds, bool transition = true)
+    {
+        /*
+        _FrontTex("Front (+Z)", 2D) = "white" {}
+		_BackTex("Back (-Z)", 2D) = "white" {}
+		_LeftTex("Left (+X)", 2D) = "white" {}
+		_RightTex("Right (-X)", 2D) = "white" {}
+		_UpTex("Up (+Y)", 2D) = "white" {}
+		_DownTex("Down (-Y)", 2D) = "white" {}
+		_FrontTex2("2 Front (+Z)", 2D) = "white" {}
+		_BackTex2("2 Back (-Z)", 2D) = "white" {}
+		_LeftTex2("2 Left (+X)", 2D) = "white" {}
+		_RightTex2("2 Right (-X)", 2D) = "white" {}
+		_UpTex2("2 Up (+Y)", 2D) = "white" {}
+		_DownTex2("2 Down (-Y)", 2D) = "white" {}
+        */
+
+        // the end of the texture becomes the start of the shader
+        RenderSettings.skybox.SetTexture("_FrontTex", RenderSettings.skybox.GetTexture("_FrontTex2"));
+        RenderSettings.skybox.SetTexture("_BackTex", RenderSettings.skybox.GetTexture("_BackTex2"));
+        RenderSettings.skybox.SetTexture("_LeftTex", RenderSettings.skybox.GetTexture("_LeftTex2"));
+        RenderSettings.skybox.SetTexture("_RightTex", RenderSettings.skybox.GetTexture("_RightTex2"));
+        RenderSettings.skybox.SetTexture("_UpTex", RenderSettings.skybox.GetTexture("_UpTex2"));
+        RenderSettings.skybox.SetTexture("_DownTex", RenderSettings.skybox.GetTexture("_DownTex2"));
+        
+        // new texture at the end of the shader
+        RenderSettings.skybox.SetTexture("_FrontTex2", ds.front);
+        RenderSettings.skybox.SetTexture("_BackTex2", ds.back);
+        RenderSettings.skybox.SetTexture("_LeftTex2", ds.left);
+        RenderSettings.skybox.SetTexture("_RightTex2", ds.right);
+        RenderSettings.skybox.SetTexture("_UpTex2", ds.up);
+        RenderSettings.skybox.SetTexture("_DownTex2", ds.down);
+
+        if(transition)
+        {
+            StopCoroutine(SkyboxFadeCoroutine());
+            StartCoroutine(SkyboxFadeCoroutine());
+        } else
+        {
+            RenderSettings.skybox.SetFloat("_Blend", 1f);
+        }
+    }
+
+    IEnumerator SkyboxFadeCoroutine(bool fadeIn = true)
+    {
+        float i;
+
+        if (fadeIn)
+            i = 0f;
+        else
+            i = 1f;
+        
+        RenderSettings.skybox.SetFloat("_Blend", i);
+
+        if(fadeIn)
+        {
+            while (i < 1f)
+            {
+                i += Time.deltaTime;
+                RenderSettings.skybox.SetFloat("_Blend", i);
+                yield return null;
+            }
+        } else
+        {
+            while (i > 0f)
+            {
+                i -= Time.deltaTime;
+                RenderSettings.skybox.SetFloat("_Blend", i);
+                yield return null;
+            }
+        }
+
+        if(fadeIn)
+            RenderSettings.skybox.SetFloat("_Blend", 1f);
+        else
+            RenderSettings.skybox.SetFloat("_Blend", 0f);
+    }
 }
---
+++ Assets/SeaPollutionGame/Source/Skybox/SkyboxBlender.cs
@@ -0,0 +1,24 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class SkyboxBlender : MonoBehaviour
+{
+    public Material skyboxMaterial = null;
+
+    [Range(0f, 1f)]
+    public float blend = 0f;
+
+    private void Update()
+    {
+        if(blend <= 1f)
+        {
+            blend += 0.0f * Time.deltaTime;
+            skyboxMaterial.SetFloat("_Blend", blend);
+        } else
+        {
+            blend -= 0.0f * Time.deltaTime;
+            skyboxMaterial.SetFloat("_Blend", blend);
+        }
+    }
+}

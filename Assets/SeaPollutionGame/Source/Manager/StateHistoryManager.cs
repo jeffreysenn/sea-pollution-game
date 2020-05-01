d857c0f0cd7618cd809d0fc43dce6627339e082c
Xiaoyue Chen <xiaoyue_chen@outlook.com>
2020-05-01 16:19:56 +0200

    Player history tracking and plotting

---
+++ Assets/SeaPollutionGame/Source/Manager/StateHistoryManager.cs
@@ -0,0 +1,17 @@
+﻿using System.Collections.Generic;
+
+public class StateHistoryManager : IStateHistoryManager
+{
+    private Dictionary<int, StateHistory> histories = new Dictionary<int, StateHistory> { };
+
+    public void RegisterPlayer(int id)
+    {
+        histories.Add(id, new StateHistory { });
+    }
+
+    public StateHistory GetStateHistory(int id)
+    {
+        return histories[id];
+    }
+}
+
---
+++ Assets/SeaPollutionGame/Source/Player/PlayerState/PlayerStateBlackboard.cs
@@ -0,0 +1,75 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Diagnostics;
+using System.Threading.Tasks;
+using UnityEngine.Events;
+
+public class PlayerStateBlackboard
+{
+    public enum Key
+    {
+        MONEY,
+        ASSET_VALUE,
+        EFFICIENCY,
+        SCORE,
+        GOAL_BONUS,
+        POLLUTION,
+        RESOURCE,
+    }
+
+    private static Key kNonValueKeys = Key.POLLUTION | Key.RESOURCE;
+    private Dictionary<Key, float> valueMap = new Dictionary<Key, float>();
+    private Dictionary<PollutionMapType, PollutionMap> accumulatedPollutionMap = new Dictionary<PollutionMapType, PollutionMap> { };
+    private ResourceMap accumulatedResourceMap = new ResourceMap { };
+
+    public void Write(Key key, float val)
+    {
+        Debug.Assert((key & kNonValueKeys) != key);
+        valueMap[key] = val;
+    }
+
+    public void Write(Key key, PollutionMapType pollutionMapType, string pollutionName, float val)
+    {
+        Debug.Assert(key == Key.POLLUTION);
+        if (!accumulatedPollutionMap.ContainsKey(pollutionMapType))
+        {
+            accumulatedPollutionMap[pollutionMapType] = new PollutionMap { };
+        }
+        accumulatedPollutionMap[pollutionMapType][pollutionName] = val;
+    }
+
+    public void Write(Key key, string resourceName, float val)
+    {
+        Debug.Assert(key == Key.RESOURCE);
+        accumulatedResourceMap[resourceName] = val;
+    }
+
+    public float GetValue(Key key)
+    {
+        Debug.Assert((key & kNonValueKeys) != key);
+        return valueMap[key];
+    }
+
+    public float GetValue(Key key, PollutionMapType pollutionMapType, string pollutionName)
+    {
+        Debug.Assert(key == Key.POLLUTION);
+        PollutionMap pollutionMap = null;
+        var value = 0.0f;
+        if (accumulatedPollutionMap.TryGetValue(pollutionMapType, out pollutionMap))
+        {
+            pollutionMap.TryGetValue(pollutionName, out value);
+        }
+        return value;
+    }
+
+    public float GetValue(Key key, string resourceName)
+    {
+        Debug.Assert(key == Key.RESOURCE);
+        var value = 0.0f;
+        accumulatedResourceMap.TryGetValue(resourceName, out value);
+        return value;
+    }
+
+}
